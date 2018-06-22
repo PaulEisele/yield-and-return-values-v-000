b1950b45a30010ecc51d9a4d91c42b55b3fb064e
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-22 02:31:42 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,4 +1,4 @@
-require pry
+
 def hello(array)
   i = 0
   collection = []
@@ -9,5 +9,5 @@ def hello(array)
   collection
 end
 
-binding.pry
+
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
